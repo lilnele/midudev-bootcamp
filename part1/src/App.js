b4639439118a63634f9b9bba902d8669ce4cde04
lilnele <91592056+lilnele@users.noreply.github.com>
2024-04-20 08:27:26 +0900

    proyecto

---
+++ part1/src/App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+import Mensaje from './Mensaje'; 
+
+
+const Description = () =>{
+
+
+return <h1>Estas es la app del curso</h1>
+
+}
+
+const Hola = () => {
+return <h1>Hola hola</h1>
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Mensaje message='Estamos trabajando'/>
+      <Mensaje message='en un curso'/>
+      <Mensaje message='de React'/>
+      <Description/>
+    </div>
+  );
+}
+
+export default App;
